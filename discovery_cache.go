795eb6df55fdb49d5ddfff43f036a634e4a22347
Sandro Santilli <strk@kbt.io>
2017-02-20 19:13:22 +0100

    Provide TimedDiscoveryCache
    Fixes #45

--- discovery_cache.go
+++ discovery_cache.go
@@ -2,6 +2,7 @@ package openid
 
 import (
 	"sync"
+	"time"
 )
 
 type DiscoveredInfo interface {
@@ -66,4 +67,47 @@ func compareDiscoveredInfo(a DiscoveredInfo, opEndpoint, opLocalID, claimedID st
 		a.OpEndpoint() == opEndpoint &&
 		a.OpLocalID() == opLocalID &&
 		a.ClaimedID() == claimedID
+}
+
+type timedDiscoveredInfo struct {
+	info DiscoveredInfo
+	time time.Time
+}
+
+type TimedDiscoveryCache struct {
+	cache map[string]timedDiscoveredInfo
+	ttl time.Duration
+	mutex *sync.Mutex
+}
+
+func NewTimedDiscoveryCache(ttl time.Duration) *TimedDiscoveryCache {
+	return &TimedDiscoveryCache{cache: map[string]timedDiscoveredInfo{}, ttl: ttl, mutex: &sync.Mutex{}}
+}
+
+func (s *TimedDiscoveryCache) Put(id string, info DiscoveredInfo) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.cache[id] = timedDiscoveredInfo{info:info, time:time.Now()}
+}
+
+func (s *TimedDiscoveryCache) Get(id string) DiscoveredInfo {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	// Delete old cached while we are at it.
+	newCache := map[string]timedDiscoveredInfo{}
+	now := time.Now()
+	for k, e := range s.cache {
+		diff := now.Sub(e.time)
+		if diff <= s.ttl {
+			newCache[k] = e
+		}
+	}
+	s.cache = newCache
+
+	if info, has := s.cache[id]; has {
+		return info.info
+	}
+	return nil
 }
